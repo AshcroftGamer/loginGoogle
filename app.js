9b57cc4b45b30d2e8af61e65b859330bac8ed677
Felipe Goes <34778191+AshcroftGamer@users.noreply.github.com>
2021-08-16 15:11:50 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express  = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+global.__basedir = __dirname;
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+const rotalogin = require('./route/routelogin');
+const rotalogout = require('./route/rotalogout');
+const rotaperfil = require('./route/rotaperfil');
+const rotapro = require('./route/rotapro');
+const rotahome = require('./route/home');
+
+app.use('/home', rotahome);
+app.use('/pro', rotapro);
+app.use('/perfil', rotaperfil);
+app.use('/logout', rotalogout);
+app.use('/login', rotalogin);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+module.exports = app;
